c5d39c8337a4b5f7537fd466c7dcf6f678161359
Luiz-Otavio Zorzella <zorzella@google.com>
2009-05-06 16:28:51 -0700

    Show/hide columns

--- src/com/zorzella/tamandua/ListServlet.java
+++ src/com/zorzella/tamandua/ListServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.util.Collection;
+import java.util.Map;
 import java.util.logging.Logger;
 
 import javax.jdo.PersistenceManager;
@@ -27,26 +28,93 @@ public class ListServlet extends HttpServlet {
     try {
 
       Html.htmlHeadBody(ps);
+
+      @SuppressWarnings("unchecked")
+      Map<String,String[]> map = req.getParameterMap();
+
+      String sortKey = "titulo";
+      String[] temp = map.get("sort");
+      if ((temp != null) && (temp.length == 1)) {
+        sortKey = temp[0];
+      }
       
-      ps.println("<table>" +
-            "<th>Onde</th>" +
-      		"<th>Titulo</th>" +
-            "<th>Autor</th>" +
-            "<th>Tamanho</th>" +
-            "<th>Tags</th>" +
-      		"");
+      boolean onde = map.containsKey("onde");
+      boolean titulo = map.containsKey("titulo");
+      boolean autor = map.containsKey("autor");
+      boolean tamanho = map.containsKey("tamanho");
+      boolean tags = map.containsKey("tags");
+      
+      if (!onde && !titulo && !autor && !tamanho && !tags) {
+        onde = true;
+        titulo = true;
+        tamanho = true;
+        autor = true;
+        tags = true;
+      }
+      
+      ps.println("<form action='list'>");
+      ps.println("Mostre: " +
+          checkbox(onde, "onde", "Onde (Paradeiro e Toca)") +
+          checkbox(titulo, "titulo", "Titulo") +
+          checkbox(autor, "autor", "Autor") +
+          checkbox(tamanho, "tamanho", "Tamanho") +
+          checkbox(tags, "tags", "Tags") +
+            "");
+      ps.println("<br>Ordene: " +
+          "<select name='sort'>" +
+          dropdown(sortKey, "paradeiro", "Paradeiro") +
+          dropdown(sortKey, "toca", "Toca") +
+          dropdown(sortKey, "titulo", "Titulo") +
+          dropdown(sortKey, "autor", "Autor") +
+          "</select>" +
+            "");
+      ps.println("<br><input type='submit'>");
+      
+      ps.println("<table>");
+      
+      if (onde) {
+        ps.println("<th>Onde</th>");
+      }
+      
+      if (titulo) {
+        ps.println("<th>Titulo</th>");
+      }
+      
+      if (autor) {
+        ps.println("<th>Autor</th>");
+      }
+      
+      if (tamanho) {
+        ps.println("<th>Tamanho</th>");
+      }
+      
+      if (tags) {
+        ps.println("<th>Tags</th>");
+      }
+
       Collection<Book> sortedBooks = Queries.getSortedBooks(pm);
 
       for (Book book : sortedBooks) {
-        ps.printf("<tr>" +
-        		"<td align='right'>%s</td>" +
-        		"<td>%s</td>" +
-        		"<td>%s</td>" +
-        		"<td>%s</td>" +
-        		"<td>%s</td>" +
-        		"\n", 
-            book.getParadeiro() + "&nbsp;" + book.getToca(), book.getTitulo(), book.getAutor(), 
-            book.getTamanho(), book.getTags());  
+        ps.printf("<tr>");
+        if (onde) {
+          ps.printf("<td align='right'>%s</td>", book.getParadeiro() + "&nbsp;" + book.getToca());
+        }
+        if (titulo) {
+          ps.printf("<td>%s</td>", book.getTitulo());
+        }
+        
+        if (autor) {
+          ps.printf("<td>%s</td>", book.getAutor());
+        }
+        
+        if (tamanho) {
+          ps.printf("<td>%s</td>", book.getTamanho());
+        }
+
+        if (tags) {
+          ps.printf("<td>%s</td>", book.getTags());
+        }
+        ps.print("\n"); 
       }
 
       ps.println("</body></html>");
@@ -58,5 +126,13 @@ public class ListServlet extends HttpServlet {
     } finally {
       pm.close();
     }
+  }
+
+  private String dropdown(String sortKey, String key, String label) {
+    return "<option value='" + key + "'" + (sortKey.equals(key) ? " selected" : "") + ">" + label + "</option>\n";
+  }
+
+  private String checkbox(boolean selected, String key, String label) {
+    return "<input type='checkbox' name='" + key + "'" + (selected ? " checked" : "" ) + ">" + label + "</input>\n";
   }
 }
