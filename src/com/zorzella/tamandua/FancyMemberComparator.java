88bc858cb7f67df65707c61118379098376e9ac4
Z" for Zorzella <zorzella@google.com>
2009-05-30 00:16:14 -0700

    paradeiro sorting restored

--- src/com/zorzella/tamandua/FancyMemberComparator.java
+++ src/com/zorzella/tamandua/FancyMemberComparator.java
@@ -16,39 +16,47 @@ public class FancyMemberComparator implements Comparator<Item> {
   @Override
   public int compare(Item one, Item other) {
     
-//    // Unknown paradeiro books group at the end
-//    if ((one.getParadeiro().equals("?")) && (!other.getParadeiro().equals("?"))) {
-//    	return 1;
-//    }
-//    if ((other.getParadeiro().equals("?")) && (!one.getParadeiro().equals("?"))) {
-//    	return -1;
-//    }
+    // Unknown paradeiro books group at the end
+    if ((paradeiro(one).equals("?")) && (!paradeiro(other).equals("?"))) {
+    	return 1;
+    }
+    if ((paradeiro(other).equals("?")) && (!paradeiro(one).equals("?"))) {
+    	return -1;
+    }
 
-//    // Known paradeiro books group in the beginning
-//    if ((one.getParadeiro().length() > 0) && (other.getParadeiro().length() == 0)) {
-//      return -1;
-//    }    
-//    if ((one.getParadeiro().length() == 0) && (other.getParadeiro().length() > 0)) {
-//      return 1;
-//    }
     // Known paradeiro books group in the beginning
-    if ((one.getParadeiro() != null) && (other.getParadeiro() == null)) {
+    if ((paradeiro(one).length() > 0) && (paradeiro(other).length() == 0)) {
       return -1;
     }    
-    if ((one.getParadeiro() == null) && (other.getParadeiro() != null)) {
+    if ((paradeiro(one).length() == 0) && (paradeiro(other).length() > 0)) {
       return 1;
     }
+//    // Known paradeiro books group in the beginning
+//    if ((paradeiro(one) != null) && (paradeiro(other) == null)) {
+//      return -1;
+//    }    
+//    if ((paradeiro(one) == null) && (paradeiro(other) != null)) {
+//      return 1;
+//    }
     
-    if ((one.getParadeiro() == null) && (other.getParadeiro() == null)) {
+    if ((paradeiro(one) == null) && (paradeiro(other) == null)) {
       return one.compareTo(other);
     }
     // Group the books with same paradeiro
-    int result = one.getParadeiro().compareTo(other.getParadeiro());
+    int result = paradeiro(one).compareTo(paradeiro(other));
     if (result != 0) {
       return result;
     }
     
     // All things being equal, alphabetical order
     return one.compareTo(other);
+  }
+
+  private String paradeiro(Item item) {
+    Long temp = item.getParadeiro();
+    if (temp == null) {
+      return "";
+    }
+    return paradeiroToCodeMap.get(temp);
   }
 }
